db621babc49991c07aefea8dabf93feb1091a96f
benp3837 <petruzziellbj@alumni.vcu.edu>
2026-03-10 13:44:51 -0400

    ingest items function done, possible refactor

---
+++ GamerStop/app/routers/vectordb_router.py
@@ -0,0 +1,28 @@
+from fastapi import APIRouter
+from pydantic import BaseModel
+
+from app.services.vectordb_service import ingest_json
+
+router = APIRouter(
+    prefix="/vectordb",
+    tags=["vectordb"]
+)
+
+# Pydantic Model for ingesting JSON items
+class IngestItem(BaseModel):
+    id: str
+    text: str
+    metadata: dict | None = None # Optional field ( = None)
+
+
+# Ingest JSON endpoint
+@router.post("/ingest-json")
+async def ingest_json_items(items:list[IngestItem]):
+
+    # Call the service method that ingests JSON
+    ingested_items = ingest_json(
+        collection_name="video_games",
+        items=[item.model_dump() for item in items]
+    )
+
+    return {"ingested_items": ingested_items}
--- GamerStop/app/services/vectordb_service.py
+++ GamerStop/app/services/vectordb_service.py
@@ -1,6 +1,7 @@
 from typing import Any
 
 from langchain_chroma import Chroma
+from langchain_core.documents import Document
 from langchain_ollama import OllamaEmbeddings
 
 # This Service is full of functions that let our VectorDB work
@@ -14,8 +15,9 @@ EMBEDDING_MODEL = OllamaEmbeddings(model="nomic-embed-text")
 # The Chroma store itself (which contains the collections)
 vector_store: dict[str, Chroma] = {}
 
-# Create a new collection in VectorDB - a collection in VDB is like a table in SQL
-def create_collection(collection_name:str):
+# Create a new collection in VectorDB - a collection in VDB is like a table in SQL.
+# If the collection already exists, just return it.
+def create_or_get_collection(collection_name:str):
 
     # If the Collection doesn't already exist, make a new one
     if collection_name not in vector_store:
@@ -27,18 +29,26 @@ def create_collection(collection_name:str):
 
     return vector_store[collection_name]
 
-
 # Ingest JSON items into the DB
 def ingest_json(collection_name:str, items:list[dict[str, Any]]):
 
     # Get an instance of the collection in the vector store
+    collection = create_or_get_collection(collection_name)
 
     # Turn the list of items into a list of document to get ingested
+    docs = [
+        Document(page_content=item["text"], metadata=item.get("metadata", {}))
+        for item in items
+    ]
 
     # Attach IDs to each document (easy cuz it's the sample data)
+    ids = [item["id"] for item in items]
 
     # Add the documents, perform the ingestion, and we have embedded vectors!
+    collection.add_documents(docs, ids=ids)
 
+    # Return the length of the ingested items just for visibility
+    return len(items)
 
 # TODO: ingest_text
 
