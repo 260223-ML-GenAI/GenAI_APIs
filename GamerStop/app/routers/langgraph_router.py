82d79b8384034fbbc12b4fd5a032e9cd661237b3
benp3837 <petruzziellbj@alumni.vcu.edu>
2026-03-19 11:13:06 -0400

    router set for basic langgraph

---
+++ GamerStop/app/routers/langgraph_router.py
@@ -0,0 +1,29 @@
+from fastapi import APIRouter
+from pydantic import BaseModel
+
+from app.services.langgraph_service import graph
+
+router = APIRouter(
+    prefix="/langgraph",
+    tags=["langgraph"]
+)
+
+# Basic Pydantic Model to handle user input
+class ChatRequest(BaseModel):
+    input: str
+
+
+# Notice how we just have a single endpoint
+# LangGraph is inherently multi-modal.
+# All that complicated Service logic cleans up our routers
+@router.post("/langgraph")
+async def langgraph_chat(chat:ChatRequest):
+
+    # Get the result of the invocation
+    result = graph.invoke({"query":chat.input})
+
+    # Return the result and the route (just to see it)
+    return {
+        "response": result.get("answer"),
+        "route": result.get("route")
+    }
