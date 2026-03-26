aed7cb4df4eae864692433f6f8d3c9c459d22ef0
benp3837 <petruzziellbj@alumni.vcu.edu>
2026-03-26 11:43:28 -0400

    working summarizer

--- GamerStop/app/pages/st_summarize.py
+++ GamerStop/app/pages/st_summarize.py
@@ -2,7 +2,7 @@ from io import StringIO
 
 import streamlit as st
 
-from app.services.langchain_service import get_basic_chain
+from services.langchain_service import get_basic_chain
 
 # Grab basic chain from service
 basic_chain = get_basic_chain()
@@ -12,18 +12,15 @@ st.header("Summarize Text Demo")
 uploaded_file = st.file_uploader("Choose a TXT file:")
 
 if uploaded_file is not None:
-    # To read file as bytes:
-    bytes_data = uploaded_file.getvalue()
-    st.write(bytes_data)
 
     # To convert to a string based IO:
     stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-    st.write(stringio)
 
-    # To read file as string:
-    string_data = stringio.read()
-    st.write(string_data)
+    # Prompt to tell the LLM to summarize the text:
+    prompt = f"Summarize this text, neutral on perspective: {stringio.read()}"
 
-    # Popup with the uploaded TXT content
-    st.warning("Here's the content of your uploaded file:")
-    st.code(string_data)
+    # Invoke the chain with the prompt and display the response
+    with st.spinner("Summarizing..."):
+        response = basic_chain.invoke(input=prompt)
+    st.subheader("Summary:")
+    st.write(response.content)
